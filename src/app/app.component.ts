f72e6c82b9cfa6603ac0b4d02562c1ee6cbbe997
lovranitslaptop <tamas198601@gmail.com>
2019-04-20 17:22:25 +0200

    Create the landing page - modify the global scss style according to the basic state (without angular, only with bootsrap use)
    Install bootstrap and ngx-bootsrap with scss
    - npm install ngx-bootstrap bootstrap --save
    - modify the angular.js file
      add this line to architect/build/options/styles block
      "node_modules/bootstrap/dist/css/bootstrap.min.css"
    
    Create the carousel function
    - npm install @ngu/carousel (not in used)
    - modify app.module.ts file
      add and import the CarouselModule from 'ngx-bootstrap/carousel'
    - modify app.component.ts (providers section in @Component dekorator)
    - modify the global scss style
    - set the ngStyle backgrounds in the app.component.html file
    
    Back to top function
    - ng g c scroll-top
    - import the component in app.module.ts
    - modify the .ts,.scss,.html file
    
    Add font awesome to project
    - npm font-awesome --save
    - modify the angular.js file
      add this line to architect/build/options/styles block
      {"input": "node_modules/font-awesome/css/font-awesome.css", "lazy":  false}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 5000, noPause: true, showIndicators: true } }
+  ]
 })
 export class AppComponent {
   title = 'gtfoto-angular';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollTopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
