8ec7da8d640756ee640a07218ffb5580bd34d919
lovranitslaptop <tamas198601@gmail.com>
2019-04-25 17:46:51 +0200

    Refactor the landing page - modify the card deck - change the isCollapsed default value to true - hide the scroll-to-top and carosel-control under sm size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import {Component} from '@angular/core';
+import {CarouselConfig} from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   providers: [
-    { provide: CarouselConfig, useValue: { interval: 5000, noPause: true, showIndicators: true } }
+    {provide: CarouselConfig, useValue: {interval: 5000, noPause: true, showIndicators: true}}
   ]
 })
 export class AppComponent {
   title = 'gtfoto-angular';
-  public isCollapsed = false;
+  public isCollapsed = true;
 
   items: string[] = [
     'Esküvő',
@@ -24,11 +24,12 @@ export class AppComponent {
   onHidden(): void {
     console.log('Dropdown is hidden');
   }
+
   onShown(): void {
     console.log('Dropdown is shown');
   }
+
   isOpenChange(): void {
     console.log('Dropdown state is changed');
   }
-
 }
