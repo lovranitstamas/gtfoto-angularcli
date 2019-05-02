e31c20ef3ffc60fbd323996aa4580e023a4f8ec9
lovranitslaptop <tamas198601@gmail.com>
2019-05-02 14:28:47 +0200

    Add the portfolio path (with engaged-list) to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {HomeComponent} from './home/home.component';
+import {EngagedListComponent} from './portfolio/engaged/engaged-list/engaged-list.component';
 import {MyserviceComponent} from './myservice/myservice.component';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
@@ -8,6 +9,12 @@ import {PageNotFoundComponent} from './core/page-not-found/page-not-found.compon
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {
+    path: 'portfolio',
+    children: [
+      {path: 'engaged-list', component: EngagedListComponent},
+    ]
+  },
   {path: 'myservice', component: MyserviceComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
