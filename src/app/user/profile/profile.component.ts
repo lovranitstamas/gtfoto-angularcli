de0e319158ae46e125a71a546e33115a623c0cc4
lovranitslaptop <tamas198601@gmail.com>
2019-05-13 20:14:27 +0200

    Show the valid mocked after login

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,9 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../shared/user.service';
+import {UserModel} from '../../shared/user-model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: UserModel;
+
+  constructor(private _userService: UserService) { }
+
+  ngOnInit() {
+    this.user = this._userService.getCurrentUser();
+  }
 }
